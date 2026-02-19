1058f174eeb7b2bfcad3092486a1d900843e20fa
tsseo <tsseo.dev@gmail.com>
2026-02-19 12:43:58 +0900

    BOJ 32401 - ANA는 회문이야 (Bronze)
    풀이: A, N 인덱스 배열 추출 → 인접 A 쌍 windowed 순회 → 사이에 N이 정확히 1개면 count

--- src/main/baekjoon/bronze/P32401.kt
+++ src/main/baekjoon/bronze/P32401.kt
@@ -10,6 +10,29 @@ fun main() = with(System.`in`.bufferedReader()) {
     val sb = StringBuilder()
 
     // 풀이 작성
+    val length = readLine()!!.trim().toInt()
+    val string = readLine()!!.trim()
 
-    print(sb)
+    var result = 0
+
+    val aIndex = mutableListOf<Int>()
+    val nIndex = mutableListOf<Int>()
+
+    string.forEachIndexed { i, char ->
+        if (char == 'A') aIndex.add(i)
+        if (char == 'N') nIndex.add(i)
+    }
+
+    // 2개씩 슬라이딩 윈도우
+    aIndex.windowed(2).forEach { (left, right) ->
+        if (left + 1 == right) {
+            return@forEach
+        }
+
+        // nIndex에서 'left < n && n < right' count
+        val nCount = nIndex.count { it in (left + 1)..<right }
+        if (nCount == 1) result++
+    }
+
+    print(result)
 }
