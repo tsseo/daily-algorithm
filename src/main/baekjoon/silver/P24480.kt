93a7a4a4aca8a33c3657f523ece837472f30a1b3
tsseo <tsseo.dev@gmail.com>
2026-02-28 23:58:19 +0900

    feat: 24480 문제 템플릿 추가

---
+++ src/main/baekjoon/silver/P24480.kt
@@ -0,0 +1,15 @@
+package main.baekjoon.silver
+
+/**
+ * BOJ 24480 - 알고리즘 수업 - 깊이 우선 탐색 2
+ * https://www.acmicpc.net/problem/24480
+ *
+ * 분류: DFS, 그래프 탐색, 정렬
+ */
+fun main() = with(System.`in`.bufferedReader()) {
+    val sb = StringBuilder()
+
+    // 풀이 작성
+
+    print(sb)
+}
